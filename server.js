b633c611c29d30950cf4f2b0ae3b7e2f7464ea0a
Oliver Tribe <oliver.lh.tribe@gmail.com>
2021-08-22 14:55:52 +1000

    set hosting environment

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/', routes);
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+if(process.env.NODE_ENV !== 'production'){
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+  });
+}
